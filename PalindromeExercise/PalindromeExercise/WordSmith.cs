2ccca04073a0ee2b81553f1d28e1176a4c4323c4
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-04-04 12:55:34 -0700

    inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+   public class WordSmith
+    {
+        public bool IsAPalindrome(string strWord)
+        {
+            var reversed = "";// will fail if space between double quotes
+           for (int x = strWord.Length -1; x >= 0; x--) //l
+            {
+                reversed += strWord[x]; //current position of strWord stored to reversed
+            }
+            if(reversed == strWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise; //header
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+     
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hi", false)]
+        [InlineData("eye", true)]
+        public void Tests(string strWord, bool expected)
         {
+            //Arrage
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(strWord);
+            //Assert
+            Assert.Equal(expected, actual);
+
+
 
         }
     }
